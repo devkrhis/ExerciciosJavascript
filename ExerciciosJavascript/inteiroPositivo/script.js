819348912c791f7fc20b1b7936add4ae0631921e
krhistopher <krhiskk@gmail.com>
2022-07-02 19:40:33 -0300

    sobe

--- ExerciciosJavascript/inteiroPositivo/script.js
+++ ExerciciosJavascript/inteiroPositivo/script.js
@@ -1,25 +1,32 @@
 // Crie uma função que recebe um inteiro positivo e teste para saber se ele é primo ou não. Faça um script que recebe um inteiro n e mostra todos os primos, de 1 até n.
 function getDados (){
-    let giveMeDados = Number(document.querySelector('#randomNumber').value);
-    console.log(giveMeDados)
+    let getNumber = Number(document.querySelector('#randomNumber').value);
+    console.log(getNumber)
 
-    receiveWhole(giveMeDados)
+    receiveWhole(getNumber)
     
 }
 
-function receiveWhole(giveMeDados){
-    let primo = 0;
-    let primo1 = 0;
+function receiveWhole(getNumber){
+    let n = 0
 
-    if(giveMeDados < 0){
+    if(getNumber < 0){
         console.log('Por gentileza informe um numero positivo')
-    } else if (giveMeDados > 1){
-        console.log("não dá pra fazer com divisão.")
+    } else if (getNumber > 1){
+        for (n = 2; n < getNumber; n++){
+            if(getNumber % n == 1){
+                console.log("O numero é primo")
+                break;
+            } else {
+                console.log("O numero não é primo")
+                break;
+            }
+
+        }
     } else {
         console.log('Isto não é um numero!')
     }
     
-
 }
 
 // function showAll (){
