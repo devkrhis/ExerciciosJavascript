68246ce9a5d499b86adff301094ca07e43755594
krhistopher <krhiskk@gmail.com>
2022-07-06 09:41:33 -0300

    ok finalizado

--- ExerciciosJavascript/QuocienteEoRestoDaDivisao/script.js
+++ ExerciciosJavascript/QuocienteEoRestoDaDivisao/script.js
@@ -1,1 +1,16 @@
 // 6. Ler dois números inteiros e exibir o quociente e o resto da divisão inteira entre eles.
+
+function getValue (){
+    let getPrimaryNumber = Number(document.querySelector('#primaryNumber').value);
+    let getSecondNumber = Number(document.querySelector('#secondNumber').value);
+
+    console.log(getPrimaryNumber, getSecondNumber);
+    calcValues(getPrimaryNumber, getSecondNumber)
+}
+
+function calcValues (setPrimaryNumber, setSecondNumber){
+    let calcQuociente = setPrimaryNumber / setSecondNumber
+    let calcRestoDivisao = setPrimaryNumber % setSecondNumber
+    document.getElementById('returnValue').innerHTML = (`${setPrimaryNumber} / ${setSecondNumber}, o resto da divisão é: ${calcRestoDivisao}, o quociente da divisão é ${calcQuociente}.`)
+    console.log(calcQuociente, calcRestoDivisao)
+}
