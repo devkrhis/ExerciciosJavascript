c4d56f1b5d1c00be0e02bedd52650c32dbc9bb44
krhistopher <krhiskk@gmail.com>
2022-07-08 17:31:36 -0300

    sobe tudo

--- ExerciciosJavascript/ConverterParaBinario/script.js
+++ ExerciciosJavascript/ConverterParaBinario/script.js
@@ -13,23 +13,20 @@ function scriptBinario(setBinario){
 
     let calcDivBinario = setBinario;
     console.log(calcDivBinario)
-    let calcA = '';
     let calcB = '';
 
     for(i=0; calcDivBinario >= 1; i++){
         let teste = calcDivBinario
         calcDivBinario = Math.floor(calcDivBinario / 2)
-        console.log("Aqui está o calcDivBinario", calcDivBinario)
 
         let teste2 = Math.round(teste) % 2
         console.log('teste',teste2)
-        console.log("Valor do calcdivBinario = ", calcDivBinario)
         if(teste2 === 0 || teste2 === 1){
             calcB += teste2
             console.log("Cheguei até aqui no CALcB:", calcB)
         }
-        console.log("Aqui está o CalcA", Math.floor(calcA))
-        
+
+        document.getElementById('returnValue').innerHTML = calcB.split("").reverse().join("");   
     }
 
 }
